dca9552980390da73ac4d6dbe9a6749ba0692a03
Dimon82 <dimon82@ya.ru>
2025-02-26 23:44:18 +0300

    Поправил checkstyle

--- hw10/src/main/java/ru/otus/hw/controller/ExceptionController.java
+++ hw10/src/main/java/ru/otus/hw/controller/ExceptionController.java
@@ -1,18 +1,13 @@
 package ru.otus.hw.controller;
 
 import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.otus.hw.exceptions.EntityNotFoundException;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionController {
