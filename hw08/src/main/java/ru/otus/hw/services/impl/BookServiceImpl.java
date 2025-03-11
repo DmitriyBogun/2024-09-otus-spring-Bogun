713557bd12f979461011d510c985a4baa160e64a
Dimon82 <dimon82@ya.ru>
2025-03-11 19:24:28 +0300

    Добавил тесты для сервиса книг

--- hw08/src/main/java/ru/otus/hw/services/impl/BookServiceImpl.java
+++ hw08/src/main/java/ru/otus/hw/services/impl/BookServiceImpl.java
@@ -2,7 +2,6 @@ package ru.otus.hw.services.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.otus.hw.exceptions.EntityNotFoundException;
 import ru.otus.hw.models.Author;
 import ru.otus.hw.models.Book;
